c855e151905918eef8bb5a1737579c06330f02fa
Brett Osiewicz <brosie00@users.noreply.github.com>
2017-11-06 11:29:23 -0500

    Create FileServerMigration.psm1
    New Module Name

---
+++ FileServerMigration.psm1
@@ -0,0 +1,5 @@
+ Get-ChildItem $psscriptroot\*.ps1 | ForEach-Object -Process { . $_.FullName }
+
+Update-TypeData -TypeName 'System.IO.DirectoryInfo' -MemberType ScriptProperty -MemberName 'isMetaDataFile' -Value { if ( ( Get-item $this ).GetFiles() -match 'DirectoryMetaDataFile.txt' ) {$True} else {$false} }
+
+ 
