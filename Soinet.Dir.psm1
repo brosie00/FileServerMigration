b1d6326edad7a598bf886eaed2438fe33d4c304e
Brett Osiewicz <brosie00@users.noreply.github.com>
2017-11-06 19:35:08 -0500

    Delete Soinet.Dir.psm1

--- Soinet.Dir.psm1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem $psscriptroot\*.ps1 | ForEach-Object -Process { . $_.FullName }
-
-Update-TypeData -TypeName 'System.IO.DirectoryInfo' -MemberType ScriptProperty -MemberName 'isMetaDataFile' -Value { if ( ( Get-item $this ).GetFiles() -match 'DirectoryMetaDataFile.txt' ) {$True} else {$false} }
-
