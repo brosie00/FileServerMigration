495ba596f4af3de66102a5169f09dc0d08e8ac63
Brett Osiewicz <brosie00@users.noreply.github.com>
2017-11-17 16:32:48 -0500

    Fixed a general formatting problem and fixed the function-ending bracket

--- New-FsmAccessDescription.ps1
+++ New-FsmAccessDescription.ps1
@@ -19,32 +19,36 @@ IsContainer      : True
 .EXAMPLE        The Output can be formatted with Format-Table. However, it should first be sorted so that the property is properly seperated.        
 Show-ACL -Path $pwd | select Path, Account, IsContainer, FullControl, Modify, ReadAndExecute, IsInherited, InheritanceFlags | Sort-Object -Property Modify | ft -GroupBy Modify #>
 function New-FsmAccessDescription {
-[CmdletBinding()]    
-Param    (        [Parameter(ValueFromPipelineByPropertyName = $true)]    
-[Alias('Filename', 'File', 'FullName', 'PsPath', 'Name', 'Directory')]        
-[string[]]       
-$Path = 'C:\Users\SWWCB\Documents'    )      
-Begin {     }      
-Process {         
-foreach ( $PathString in $Path ) {  
-$PathString = Resolve-Path -Path $PathString    
-$AccessRule = Get-Acl -Path $PathString |                
-Select-Object -ExpandProperty Access |                
-Get-FsmFileSystemAccess                    
-Foreach ($Rule in $AccessRule) {                
-$Props = @{}                
-$Props.Path = $PathString                
-$Props.Account = $Rule.Account               
-$Props.ReadAndExecute = $Rule.ReadAndExecute               
-$Props.FullControl = $Rule.FullControl                
-$Props.IsInherited = $Rule.IsInherited              
-$Props.InheritanceFlags = $Rule.InheritanceFlags               
-$Props.Modify = $Rule.Modify                                
+    [CmdletBinding()]    
+    Param    (        [Parameter(ValueFromPipelineByPropertyName = $true)]    
+        [Alias('Filename', 'File', 'FullName', 'PsPath', 'Name', 'Directory')]        
+        [string[]]       
+        $Path = 'C:\Users\SWWCB\Documents'    )      
+    Begin {     }      
+    Process {         
+        foreach ( $PathString in $Path ) {  
+            $PathString = Resolve-Path -Path $PathString    
+            $AccessRule = Get-Acl -Path $PathString |                
+                Select-Object -ExpandProperty Access |                
+                Get-FsmFileSystemAccess                    
+            Foreach ($Rule in $AccessRule) {                
+                $Props = @{}                
+                $Props.Path = $PathString                
+                $Props.Account = $Rule.Account               
+                $Props.ReadAndExecute = $Rule.ReadAndExecute               
+                $Props.FullControl = $Rule.FullControl                
+                $Props.IsInherited = $Rule.IsInherited              
+                $Props.InheritanceFlags = $Rule.InheritanceFlags               
+                $Props.Modify = $Rule.Modify                                
 
 
                 $Description = New-Object -TypeName PSObject -Property $Props                
-                Write-Output  -InputObject $Description            }        }    }   
-                End { } #end End} #end function 
+                Write-Output  -InputObject $Description            
+            }        
+        }    
+    }   
+    End { } #end End
+} #end function 
 
 
 
